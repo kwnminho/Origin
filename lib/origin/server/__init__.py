20f1b8a81d2040cbfa76be75d9b51e585bdfdeb6
Matthew Ebert <mfe5003@gmail.com>
2017-05-21 02:22:06 -0500

    moved read socket handler to another thread since it was blocking other sockets, fixed a bug in hdf5 get archived data

--- lib/origin/server/__init__.py
+++ lib/origin/server/__init__.py
@@ -12,5 +12,5 @@ from origin.server.origin_destination import Destination
 # if you dont want to install these modules then just comment the ones you dont want to use
 from origin.server.origin_hdf5_destination import HDF5Destination
 #from origin.server.origin_mysql_destination import MySQLDestination
-from origin.server.origin_filesystem_destination import FilesystemDestination
-from origin.server.origin_mongodb_destination import MongoDBDestination
+#from origin.server.origin_filesystem_destination import FilesystemDestination
+#from origin.server.origin_mongodb_destination import MongoDBDestination
