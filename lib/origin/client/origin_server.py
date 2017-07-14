bf667c6bdf7d1013f25ebb8855701f67a0b64f53
Matt Ebert <mfe5003@gmail.com>
2017-07-14 13:17:29 -0500

    fixed issue #16, edited for pep8

--- lib/origin/client/origin_server.py
+++ lib/origin/client/origin_server.py
@@ -7,10 +7,9 @@ from origin import data_types, registration_validation
 
 
 import zmq
-import sys
-import string
 import struct
 import json
+
 
 def decode(measurementType):
     try:
@@ -26,29 +25,36 @@ def decode(measurementType):
         return "string"
     return None
 
-def declarationFormater(stream,records,keyOrder):
+
+def declarationFormater(stream, records, keyOrder):
     decStr = [stream]
     for key in keyOrder:
-        decStr.append(':'.join([key,records[key]]))
+        decStr.append(':'.join([key, records[key]]))
     return ','.join(decStr)
 
 
-def formatStreamDeclaration(stream,records,keyOrder,format):
+def formatStreamDeclaration(stream, records, keyOrder, format):
     measurements = records.keys()
     sentDict = {}
     for m in measurements:
         decodedType = decode(records[m])
-        if decodedType == None:
+        if decodedType is None:
             print "Programming error. Error should be caught before this"
             return None
         else:
             sentDict[m] = decodedType
     if (format is not None) and (format.lower() == "json"):
         if keyOrder is not None:
-            print "Warning: JSON formatting selected and a key order has been defined. JSON object order is not gaurenteed therefore it is not recommended to use binary data packets."
-        return json.dumps((stream,sentDict), sort_keys=True) # make deterministic
+            msg = (
+                "Warning: JSON formatting selected and a key order has been "
+                "defined. JSON object order is not gaurenteed therefore it is "
+                "not recommended to use binary data packets."
+            )
+            print msg
+        # make deterministic
+        return json.dumps((stream, sentDict), sort_keys=True)
     else:
-        return declarationFormater(stream,sentDict,keyOrder)
+        return declarationFormater(stream, sentDict, keyOrder)
 
 
 class server:
@@ -58,56 +64,57 @@ class server:
     def ping(self):
         return True
 
-    def registerStream(self,stream,records,keyOrder=None,format=None,timeout=1000):
+    def registerStream(self, stream, records, keyOrder=None, format=None, timeout=1000):
         valid = registration_validation(stream, records, keyOrder)
 
         if not valid:
             print "invalid stream declaration"
             return None
 
-        port = self.config.get('Server',"register_port")
-        msgport = self.config.get('Server',"measure_port")
-        if format=='json':
-            port = self.config.get('Server',"json_register_port")
-            msgport = self.config.get('Server',"json_measure_port")
+        port = self.config.get('Server', "register_port")
+        msgport = self.config.get('Server', "measure_port")
+        if format == 'json':
+            port = self.config.get('Server', "json_register_port")
+            msgport = self.config.get('Server', "json_measure_port")
 
         context = zmq.Context()
         socket = context.socket(zmq.REQ)
-        socket.setsockopt(zmq.RCVTIMEO,timeout)
-        socket.setsockopt(zmq.LINGER,0)  
-        host = self.config.get('Server',"ip")
-        socket.connect ("tcp://%s:%s" % (host,port))
+        socket.setsockopt(zmq.RCVTIMEO, timeout)
+        socket.setsockopt(zmq.LINGER, 0)
+        host = self.config.get('Server', "ip")
+        socket.connect("tcp://%s:%s" % (host, port))
 
         if (keyOrder is None) and (format is None):
             keyOrder = records.keys()
-        registerComm = formatStreamDeclaration(stream,records,keyOrder,format)
-        
-        if(registerComm == None):
+        registerComm = formatStreamDeclaration(stream, records, keyOrder, format)
+
+        if(registerComm is None):
             if format is None:
                 format = "comma-separated values"
             print "can't format stream into {}".format(format)
             return None
 
-        socket.send(registerComm,zmq.NOBLOCK)
+        socket.send(registerComm, zmq.NOBLOCK)
         try:
             confirmation = socket.recv()
-        except: 
+        except:
             print("Problem registering stream: {}".format(stream))
             print("Server did not respond in time")
-            exit(1)
-        returnCode, msg = confirmation.split(',',1)
+            return None
+        returnCode, msg = confirmation.split(',', 1)
         print returnCode, msg
 
         if int(returnCode) != 0:
-            print "Problem registering stream",stream
+            print "Problem registering stream", stream
             print msg
             return None
 
-        streamID, version = struct.unpack("!II",msg)
-        print("successfully registered with streamID: {}, version: {}".format(streamID,version))
-        socket.close() # I 'm pretty sure we need this here
+        streamID, version = struct.unpack("!II", msg)
+        msg = "successfully registered with streamID: {}, version: {}"
+        print(msg.format(streamID, version))
+        socket.close()  # I 'm pretty sure we need this here
 
         # error checking
         socket_data = context.socket(zmq.PUSH)
-        socket_data.connect("tcp://%s:%s"%(host,msgport))
-        return server_connection(self.config,stream,streamID,keyOrder,format,records,context,socket_data)
+        socket_data.connect("tcp://%s:%s" % (host, msgport))
+        return server_connection(self.config, stream, streamID, keyOrder, format, records, context, socket_data)
